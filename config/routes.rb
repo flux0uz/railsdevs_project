79031ba6b60e678a145878ffe3b7c0980ae1c905
Arthur Amon <61015466+flux0uz@users.noreply.github.com>
2021-04-25 15:59:14 +0200

    first commit with initial setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+  mount SimpleDiscussion::Engine => "/forum"
 
   devise_for :users
   root to: 'home#index'
