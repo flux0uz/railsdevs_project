c58b7e3dd70dca3c6da9af15667c792df90a8712
Arthur Amon <61015466+flux0uz@users.noreply.github.com>
2021-04-25 21:18:52 +0200

    create job scaffold, add active_storage and action_text

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
   mount SimpleDiscussion::Engine => "/forum"
-
+  
   devise_for :users
   root to: 'home#index'
+  resources :jobs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
